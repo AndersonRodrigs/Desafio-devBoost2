0fd75f3f2b530bbf4a1e3e660d9154e5a5ee2cd1
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-11-29 13:57:30 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const btnMobile = document.querySelector("#btn-mobile")
+const nav = document.querySelector("#nav")
+
+function toggleMenu(e) {
+  if (e.type == "touchstart") e.preventDefault()
+  nav.classList.toggle("active")
+}
+
+function closeMenu() {
+  nav.classList.remove("active")
+}
+
+btnMobile.addEventListener("click", e => {
+  toggleMenu(e)
+})
+
+btnMobile.addEventListener("touchstart", e => {
+  toggleMenu(e)
+})
+
